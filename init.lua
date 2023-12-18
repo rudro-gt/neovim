2788ee83d82500f111919158d0d13af9c73377f1
Dardaul Hoque <rudro1152@gmail.com>
2023-12-19 00:25:17 +0600

    Tab to confirm suggestion

--- init.lua
+++ init.lua
@@ -652,33 +652,40 @@ cmp.setup {
     completeopt = 'menu,menuone,noinsert',
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<Tab>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<C-j>'] = cmp.mapping(function(fallback)
+    ['<CR>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
+        cmp.confirm()
       else
         fallback()
       end
     end, { 'i', 's' }),
-    ['<C-k>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+   -- ['<C-j>'] = cmp.mapping(function(fallback)
+   --   if cmp.visible() then
+   --     cmp.select_next_item()
+   --   elseif luasnip.expand_or_locally_jumpable() then
+   --     luasnip.expand_or_jump()
+   --   else
+   --     fallback()
+   --   end
+   -- end, { 'i', 's' }),
+   -- ['<C-k>'] = cmp.mapping(function(fallback)
+   --   if cmp.visible() then
+   --     cmp.select_prev_item()
+   --   elseif luasnip.locally_jumpable(-1) then
+   --     luasnip.jump(-1)
+   --   else
+   --     fallback()
+   --   end
+   -- end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },

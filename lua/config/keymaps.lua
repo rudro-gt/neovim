d5fee8c92a7e1e87b4f539249e645c2b840e0d5e
Dardaul Hoque <rudro1152@gmail.com>
2024-10-28 03:21:14 +0600

    override terminal toggle keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,11 +3,19 @@
 -- Local keymaps can be found in $HOME/.local/share/nvim/lazy/LazyVim
 
 local map = vim.keymap.set
--- local delete = vim.keymap.del
+local delete = vim.keymap.del
 local opts = { noremap = true, silent = true }
 
 map("i", "jj", "<Esc>", opts) -- normal mode remap
 map("n", "<C-a>", "gg<S-v>G", opts) -- select all
 map("n", "K", "-J", opts) -- join with previous line
+
+-- Override terminal toggle
+delete("n", "<c-/>")
+delete("t", "<C-/>")
+map("n", "<c-`>", function() -- LazyVim.terminal() is wrapped to match expected function signature
+  LazyVim.terminal()
+end, { desc = "Terminal (Root Dir)" })
+map("t", "<C-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
 -- windows and tabs

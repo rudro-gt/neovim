f9edc7fd927b8effe3eee51dbede5a9193c2a12b
Dardaul Hoque <rudro1152@gmail.com>
2024-10-28 15:31:38 +0600

    remove colon from todo highlight matcher

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,4 +9,16 @@ return {
       },
     },
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      highlight = {
+        pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+      },
+      search = {
+        pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon
+      },
+    },
+  },
 }

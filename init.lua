5f8d3c171a24386bd9707752b33a01e8d8c2845c
Dardaul Hoque <rudro1152@gmail.com>
2024-11-11 18:30:40 +0600

    disable annoying change detection

--- init.lua
+++ init.lua
@@ -7,6 +7,9 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
   },
+  change_detection = {
+    notify = false,
+  },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.

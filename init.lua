d32e6e3a3fcbb058712f4f46d4b5338c0f6d62a3
Dardaul Hoque <rudro1152@gmail.com>
2024-11-12 15:49:03 +0600

    disable autoupdate check

--- init.lua
+++ init.lua
@@ -20,8 +20,8 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
+    enabled = false, -- check for plugin updates periodically
+    notify = true, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
     rtp = {

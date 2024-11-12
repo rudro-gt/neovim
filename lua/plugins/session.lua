ef4db8a670ef46e7eb3f33b1e4f63eaac1b7f72d
Dardaul Hoque <rudro1152@gmail.com>
2024-11-12 10:24:29 +0600

    change the session manager

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,9 +1,21 @@
 return {
   {
     "folke/persistence.nvim",
-    event = "VeryLazy",
-    init = function(_)
-      require("persistence").load()
+    enabled = false,
+  },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Dev", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+    init = function()
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
     end,
   },
 }

43b8e949600f655779e839e93dfc9dfb3fd12c51
Dardaul Hoque <rudro1152@gmail.com>
2024-11-16 11:28:42 +0600

    add more language support

--- init.lua
+++ init.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
   },
   checker = {
     enabled = false, -- check for plugin updates periodically
-    notify = true, -- notify on update
+    notify = false, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
     rtp = {
--- lua/plugins/language_service.lua
+++ lua/plugins/language_service.lua
@@ -11,5 +11,16 @@ return {
   },
 
   -- Language Support
+  { import = "lazyvim.plugins.extras.lang.astro" },
+
+  { import = "lazyvim.plugins.extras.lang.docker" },
+
   { import = "lazyvim.plugins.extras.lang.go" },
+
+  { import = "lazyvim.plugins.extras.lang.python" },
+
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
 }

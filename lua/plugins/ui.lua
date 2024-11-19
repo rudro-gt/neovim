01958b2565079af077d0fec0ca2a60c7209c92c6
Dardaul Hoque <rudro1152@gmail.com>
2024-11-19 20:17:38 +0600

    add indicators for folke/todo.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,6 +13,15 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
+      keywords = {
+        FIX = {
+          icon = " ", -- icon used for the sign, and in search results
+          color = "error", -- can be a hex color, or a named color (see below)
+          alt = { "BUG", "DEBUG", "REMOVE" }, -- a set of other keywords that all map to this FIX keywords
+          -- signs = false, -- configure signs for some keywords individually
+        },
+        TODO = { icon = " ", color = "info", alt = { "REFACTOR" } },
+      },
       highlight = {
         pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
       },

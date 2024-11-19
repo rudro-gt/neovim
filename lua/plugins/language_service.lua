6dcbc45d146015ea74a6daae929d1ee0ff67cdab
Dardaul Hoque <rudro1152@gmail.com>
2024-11-19 18:40:54 +0600

    add DAP

--- lua/plugins/language_service.lua
+++ lua/plugins/language_service.lua
@@ -11,16 +11,18 @@ return {
   },
 
   -- Language Support
-  { import = "lazyvim.plugins.extras.lang.astro" },
-
   { import = "lazyvim.plugins.extras.lang.docker" },
 
   { import = "lazyvim.plugins.extras.lang.go" },
 
   { import = "lazyvim.plugins.extras.lang.python" },
 
-  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.astro" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  -- Debug Adapter Protocol
+  { import = "lazyvim.plugins.extras.dap.core" },
 }

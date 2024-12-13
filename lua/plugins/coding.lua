ce97c8fec50776cea1ffa56d27b3b12eb5661e6d
Dardaul Hoque <rudro1152@gmail.com>
2024-12-13 16:23:00 +0600

    disable inlay hints

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -2,12 +2,15 @@ return {
   -- LSP keymap Overrides
   {
     "neovim/nvim-lspconfig",
-    opts = function()
+    init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- override hover keymap
       keys[#keys + 1] = { "K", false }
       keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Hover" }
     end,
+    opts = {
+      inlay_hints = { enabled = false },
+    },
   },
 
   -- LSP, Linters etc.

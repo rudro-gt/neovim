e101deaa224ddc844451ab8395039cf347ec3e8e
Dardaul Hoque <rudro1152@gmail.com>
2024-12-30 21:25:06 +0600

    change the markdown renderer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -50,12 +50,10 @@ return {
     },
   },
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
   },
 }

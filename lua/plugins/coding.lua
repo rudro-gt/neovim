a70d1331a68a306265a04263150956145ad84cad
Dardaul Hoque <rudro1152@gmail.com>
2025-02-09 20:28:10 +0600

    handle extra plugins from LazyExtras command

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,4 @@
 return {
-  -- LSP, Linters etc.
   {
     "neovim/nvim-lspconfig",
     init = function()
@@ -8,22 +7,6 @@ return {
       keys[#keys + 1] = { "K", false }
     end,
   },
-  { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.astro" },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-  { import = "lazyvim.plugins.extras.lang.svelte" },
-
-  -- Debug Adapter Protocol
-  { import = "lazyvim.plugins.extras.dap.core" },
-
-  -- Testing
-  { import = "lazyvim.plugins.extras.test.core" },
   {
     "nvim-neotest/neotest",
     dependencies = {

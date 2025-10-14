baae891d082560484ab1e5e2238763cc5b1b93fd
Dardaul Hoque <rudro1152@gmail.com>
2025-10-14 21:34:50 +0600

    mini.files: change synchronization key

--- lua/plugins/nav.lua
+++ lua/plugins/nav.lua
@@ -1,5 +1,52 @@
 return {
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-mini/mini.files",
+    opts = {
+      -- Module mappings created only inside explorer.
+      -- Use `''` (empty string) to not create one.
+      mappings = {
+        close = "q",
+        go_in = "L",
+        go_in_plus = "<Enter>",
+        go_out = "H",
+        go_out_plus = "<BS>",
+        mark_goto = "'",
+        mark_set = "m",
+        reset = "",
+        reveal_cwd = "@",
+        show_help = "g?",
+        synchronize = ":w",
+        trim_left = "<",
+        trim_right = ">",
+      },
+      windows = {
+        preview = true,
+        width_focus = 30,
+        width_preview = 30,
+      },
+    },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open mini.files (Directory of Current File)",
+      },
+      {
+        "<leader>E",
+        function()
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Open mini.files (cwd)",
+      },
+    },
+  },
+  {
     "ibhagwan/fzf-lua",
     keys = {
       { "<leader><leader>", false },

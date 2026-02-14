fa24d7461e6e641461a835121396933dcc47e63e
Dardaul (Rudro) Hoque <foss@rudro.me>
2026-02-15 00:00:04 +0600

    remove write on bufleave autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,18 +14,3 @@ autocmd("RecordingLeave", {
     vim.notify("Macro recorded into register " .. vim.fn.reg_recording(), "info")
   end,
 })
-
-autocmd({ "BufLeave", "FocusLost" }, {
-  pattern = "*",
-  callback = function()
-    local current_buf = vim.api.nvim_get_current_buf()
-    local buftype = vim.api.nvim_get_option_value("buftype", { buf = current_buf })
-    local isModifiable = vim.api.nvim_get_option_value("modifiable", { buf = current_buf })
-    local isValid = vim.api.nvim_buf_is_valid(current_buf)
-
-    if isModifiable and isValid and buftype ~= "nofile" and vim.bo.modified then
-      vim.cmd("silent write")
-    end
-  end,
-})
-
